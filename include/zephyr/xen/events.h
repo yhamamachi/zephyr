cacead256a76271e2e54e0975e468d73071c3093
Dmytro Firsov <dmytro_firsov@epam.com>
2023-02-14 14:40:38 +0200

    xenvm: events: add event channel allocation for domain-0
    This commit adds interface for evtchn allocation that can be used by
    privilaged domain. Domain 0 can specify both dom and remote_dom
    parameters for hypercall, where in others domains dom should be always
    DOMID_SELF. It is needed for creating pre-defined channels during
    domain setup in Zephyr Dom0.
    
    Signed-off-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- include/zephyr/xen/events.h
+++ include/zephyr/xen/events.h
@@ -37,6 +37,15 @@ int notify_evtchn(evtchn_port_t port);
 int alloc_unbound_event_channel(domid_t remote_dom);
 
 /*
+ * Allocate event-channel between remote domains. Can be used only from Dom0.
+ *
+ * @param dom - first remote domain domid (may be DOMID_SELF)
+ * @param remote_dom - second remote domain domid
+ * @return - local event channel port on success, negative on error
+ */
+int alloc_unbound_event_channel_dom0(domid_t dom, domid_t remote_dom);
+
+/*
  * Allocate local event channel, binded to remote port and attach specified callback
  * to it
  *
