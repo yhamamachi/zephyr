975862d0fc15062683f67e2054b358644df12d07
Oleksii Moisieiev <oleksii_moisieiev@epam.com>
2023-05-15 13:18:57 +0300

    drivers: xen: add Xen extended regions driver
    Introducing driver for the extended regions, provided by XEN to
    Zephyr, running as Xen Domain-0.
    Extended regions are the unmapped space in Stage 2 and is not
    bound to any guest and can be used by Zephyr for the foreign mapping.
    It configures with regs property in hypervisor node, compatible to
    xen,xen. For Example the following device-tree overlay:
    
    &hypervisor {
    	    reg = < 0x0 0x40200000 0x0 0x1000000
    	    	    0x0 0x40400000 0x0 0x1FC00000
    		    0x0 0x80000000 0x1 0x40000000>;
    };
    
    Will allocate two extended regions:
    - 0x40400000 with size 0x1fc00000
    - 0x80000000 with size 0x140000000
    
    The first region: 0x40200000 size 0x1000000 is for the grant_tables
    and will be ignored.
    
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Acked-by: Dmytro Firsov <dmytro_firsov@epam.com>

---
+++ include/zephyr/xen/regions.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023 EPAM Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef __XEN_REGIONS_H__
+#define __XEN_REGIONS_H__
+
+/**
+ * @brief Allocate pages from the extended regions
+ *
+ * @param nr_pages - number of pages to be allocated
+ * @return - pointer to the allocated pages
+ */
+void *xen_region_get_pages(size_t nr_pages);
+
+/**
+ * @brief Free pages on extended regions
+ * @param ptr - pointer to the pages, allocated by xen_region_get_pages call
+ * @param nr_pages - number of pages that were allocated
+ */
+void xen_region_put_pages(void *ptr, size_t nr_pages);
+
+#endif /* __XEN_REGIONS_H__ */
