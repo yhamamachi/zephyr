d8801ffd431724f4286f75527f8f65cd774798f7
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2025-11-11 13:45:03 +0900

    DEV: sparrow-hawk: cr52: hello-world: Check working core
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,9 +6,25 @@
 
 #include <stdio.h>
 
+#ifdef CONFIG_BOARD_RCAR_SPARROW_HAWK_R8A779G3_R52
+#include <stdint.h>
+static inline uint32_t read_mpidr(void)
+{
+	uint32_t mpidr;
+	__asm__ volatile ("mrc p15, 0, %0, c0, c0, 5" : "=r" (mpidr));
+	return mpidr;
+}
+#endif //CONFIG_BOARD_RCAR_SPARROW_HAWK_R8A779G3_R52
+
 int main(void)
 {
+#ifdef CONFIG_BOARD_RCAR_SPARROW_HAWK_R8A779G3_R52
+	uint32_t mpidr = read_mpidr();
+	uint32_t core_id = (mpidr >> 8) & 0xff;
+	printf("Hello World! %s (core_id=%d)\n", CONFIG_BOARD_TARGET, core_id);
+#else
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+#endif
 
 	return 0;
 }
